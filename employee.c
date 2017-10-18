208ebdfc580f3c5133e3960a93dfe6994d1b6ff3
student <student@localhost.localdomain>
2017-10-18 04:18:19 -0600

    git commit -m "Added comments" ichanges

--- employee.c
+++ employee.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include "employee.h"
 
+//Creates a new employee root
 struct Employee *NewEmployeeList(char *name){
   struct Employee *root = (struct Employee *)malloc(sizeof(struct Employee));
   root->name = name;
@@ -11,13 +12,16 @@ struct Employee *NewEmployeeList(char *name){
   return root;
 };
 
+//Inserts employee node in the correct BST structure
 struct Employee *insertEmployee(struct Employee *root, char *name){
   if(root == NULL){
     return NewEmployeeList(name);
   }
+  //if the name lexicographically comes before the name of the current node
   else if(strcmp(name, root->name) < 0){
     root->left = insertEmployee(root->left, name);
   }
+  //if the name lexicographically comes after the name of the current node
   else{
     root->right = insertEmployee(root->right, name);
   }
@@ -33,27 +37,30 @@ struct Employee *deleteEmployee(struct Employee *root, char *name){
   if(root == NULL){
     return root;
   }
-
+  //travels to where the root you need is
   if(strcmp(name, root->name) < 0){
     root->left = deleteEmployee(root->left, name);
   }
   else if(strcmp(name, root->name) > 0){
     root->right = deleteEmployee(root->right, name);
   }
+  //if the name is equal to the name of the node
   else{
+    //if node to be deleted only has one child
     if(root->right == NULL){
       return root->left;
     }
     if(root->left == NULL){
       return root->right;
     }
-    
+    //if node has two children
     root->name = inorderSuccessor(root->right);
     root->right = deleteEmployee(root->right, root->name);
   }
   return root;
 }
 
+//used in deleteEmployees
 char *inorderSuccessor(struct Employee *root){
   char *successor = root->name;
   while(root->left != NULL){
